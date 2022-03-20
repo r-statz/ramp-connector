7dcf6fd2648952eb7dcf4816fc9c6c0462a6e3cd
Robin Statz <robinstatz@Robins-MBP.hsd1.co.comcast.net>
2022-03-20 16:47:50 -0600

    working

--- ramp-connector-two/routes/index.js
+++ ramp-connector-two/routes/index.js
@@ -2,29 +2,26 @@ const express = require("express");
 const router = express.Router();
 const endpoints = require("../controllers/index-ctrl");
 
-router.get("/", async function (req, res, next) {
+router.get("/inventory", async function (req, res, next) {
   res.render("index", { title: "Express" });
-  // res.render("inventory-transfer", { result: 'taco' });
 });
 
-router.get("/inventory", async function (req, res, next) {
-  const result = await endpoints.inventory(req);
-  res.json({ result });
-});
+// router.get("/inventory", async function (req, res, next) {
+//   const result = await endpoints.inventory(req);
+//   res.json({ result });
+// });
 
 router.post("/inventory-transfer", async function (req, res, next) {
-  // if (req.body.skus.length === req.body.qtys.length) {
-  const result = await endpoints.inventoryTransferCheck(req);
-
-  console.log(result, "after model return");
-  // res.json({ result });
-  res.render("inventory-transfer", { result });
-  // } else {
-  //   res.render("error.ejs", {
-  //     message: "no tacos today, guys. the numbers don't match or something.",
-  //     error: "missing input data or something"
-  //   });
-  // }
+  if (req.body.qtys.length > 0 && req.body.skus.length > 0) {
+    const result = await endpoints.inventoryTransferCheck(req);
+    res.render("inventory-transfer", { result });
+  } else {
+    res.render("error.ejs", {
+      message:
+        "no tacos today, guys. the numbers don't match or something. go back and start over",
+      error: "missing input data or something"
+    });
+  }
 });
 
 module.exports = router;
